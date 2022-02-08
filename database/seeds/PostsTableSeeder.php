4d256bc81c4d3cf51bdc385d2d6d3e0d33e7e8b5
Tolihama <fra070@gmail.com>
2022-02-08 20:34:21 +0100

    PostsTableSeeder with Faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+
+// Faker import
+use Faker\Generator as Faker;
+
+// Model import
 use App\Post;
 
 class PostsTableSeeder extends Seeder
@@ -11,16 +16,15 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $data = config('posts-test');
-
-        foreach($data as $item) {
+        for ($i = 0; $i < 200; $i++) {
             $new_post = new Post();
 
-            $new_post->title = $item['title'];
+            $new_post->category_id = rand(1, 4);
+            $new_post->title = $faker->text;
             $new_post->slug = Str::slug($new_post->title, '-');
-            $new_post->content = $item['content'];
+            $new_post->content = $faker->text;
 
             $new_post->save();
         }
