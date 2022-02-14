1cdd488365446dcd94a6503cf0c0c927956cec65
Tolihama <fra070@gmail.com>
2022-02-14 15:09:53 +0100

    Welcome email feature

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendWelcomeEmail;
 
 class HomeController extends Controller
 {
@@ -11,6 +14,8 @@ class HomeController extends Controller
      * Admin Homepage
      */
     public function index() {
+        Mail::to(Auth::user()->email)->send(new SendWelcomeEmail(Auth::user()->name));
+
         return view('admin.home');
     }
 }
